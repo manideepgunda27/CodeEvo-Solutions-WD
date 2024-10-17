bef139d7f0cf684a835d8cda4c64d02b9d663e16
Gunda manideep <139052776+manideepgunda27@users.noreply.github.com>
2024-10-17 14:54:46 +0530

    Create webRTC.js

---
+++ Task-5/public/js/webRTC.js
@@ -0,0 +1,50 @@
+const socket = io('/');
+const videoGrid = document.getElementById('videoGrid');
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+let myVideoStream;
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream);
+    });
+});
+
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close();
+});
+
+const peers = {};
+
+function connectToNewUser(userId, stream) {
+    const call = peer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    });
+    call.on('close', () => {
+        video.remove();
+    });
+    peers[userId] = call;
+}
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+    videoGrid.append(video);
+}
+
+// Screen sharing
+document.getElementById('screenShare').addEventListener('click', async () => {
+    const screenStream = await navigator.mediaDevices.getDisplayMedia({ video: true });
+    const screenVideo = document.createElement('video');
+    addVideoStream(screenVideo, screenStream);
+});
